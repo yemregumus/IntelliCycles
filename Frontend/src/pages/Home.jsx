ac1bc54604d7eac9c6a62f58adafbe16141bdd73
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-20 10:36:19 -0400

    home page file created

---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    console.log("Rendering Home Page");
+    return(
+        <h2>This is the home page</h2>
+    )
+    
+}
+export default Home;
