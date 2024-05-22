7be815edab8ce2b42dd356533e7a422ca34c9205
Dev Shah <busycaesar@gmail.com>
2024-05-22 18:23:37 -0400

    Refactor(B): Server architecture

---
+++ Backend/src/server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+if (!PORT) throw err("PORT not provided.");
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
