1fd9e9bd41ca468b06111d0fef0c0d9849d3c528
Dev Shah <busycaesar@gmail.com>
2024-05-25 11:49:41 -0400

    Add(B): API route to test DB

--- Backend/src/routes/index.js
+++ Backend/src/routes/index.js
@@ -2,8 +2,21 @@ const express = require("express");
 const router = express.Router();
 const { resMessage } = require("./responseFormat");
 const info = require("../../package.json");
+const pool = require("../db");
 
 router.use("/api", require("./api"));
+
+// Temp test route
+router.get("/test", async (req, res) => {
+  try {
+    console.log("Testing DB");
+    const result = await pool.query("SELECT NOW()");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 // GET Routes.
 router.get("/", (req, res) => {
