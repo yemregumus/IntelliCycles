96aa5b65c0f2ec32830cfb1ed8f31dc60d031603
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 08:04:38 -0400

    Schedule component created

---
+++ Frontend/src/components/Schedule.jsx
@@ -0,0 +1,74 @@
+import React from "react"
+import {Container, Row, Col} from "react-bootstrap"
+
+const Schedule = () =>{
+    return(
+        <Container className="text-white text-center my-5">
+        <Row> 
+          <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">S</Col>
+          <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">M</Col>
+          <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">T</Col>
+          <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">W</Col>
+          <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">T</Col>
+          <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">F</Col>
+          <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">S</Col>
+        </Row>
+        <Row>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+        </Row>
+        <Row>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+        </Row>
+        <Row>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+        </Row>
+        <Row>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+        </Row>
+        <Row>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+        </Row>
+        <Row>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2">.</Col>
+        </Row>
+      </Container>
+    )
+}
+
+export default Schedule;
