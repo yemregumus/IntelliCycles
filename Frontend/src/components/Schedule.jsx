45838b6731007abafc62bce09c2a3542075d27a2
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 12:06:01 -0400

    + icon added to the schedule

--- Frontend/src/components/Schedule.jsx
+++ Frontend/src/components/Schedule.jsx
@@ -1,10 +1,12 @@
-import React from "react"
-import {Container, Row, Col} from "react-bootstrap"
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { IoMdAdd } from "react-icons/io";
 
-const Schedule = () =>{
-    return(
-        <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-5 h-full">
-        <Row> 
+const Schedule = () => {
+  return (
+    <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-5 h-full flex flex-col justify-between">
+      <div>
+        <Row>
           <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">S</Col>
           <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">M</Col>
           <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">T</Col>
@@ -67,8 +69,10 @@ const Schedule = () =>{
           <Col className="border-x-2 text-3xl">.</Col>
           <Col className="border-x-2 text-3xl">.</Col>
         </Row>
-      </Container>
-    )
-}
+      </div>
+      <IoMdAdd className="bg-black p-3 rounded-full mx-auto" color="white" size={80} />
+    </Container>
+  );
+};
 
 export default Schedule;
