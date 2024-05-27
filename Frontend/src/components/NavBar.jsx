16a4bc915df6d6e090b195a1725cc83dbe0ae313
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 08:03:33 -0400

    NavBar created

---
+++ Frontend/src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+const NavBar = () => {
+    return (
+        <Navbar expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">IntelliCycles</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#tasks">Tasks</Nav.Link>
+                        <Nav.Link href="#Reminders">Reminders</Nav.Link>
+                        <Nav.Link href="#Habits">Habits</Nav.Link>
+                        <Nav.Link href="#Calendar">Calendar</Nav.Link>
+                        <Nav.Link href="#Account">Account</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
