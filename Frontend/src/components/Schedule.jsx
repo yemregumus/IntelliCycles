86a17e6fa0d610e84822c74a7e0dd01c0ae3b7e8
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 14:56:51 -0400

    condensed the code for schedule component to make it more dynamic

--- Frontend/src/components/Schedule.jsx
+++ Frontend/src/components/Schedule.jsx
@@ -1,76 +1,34 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { IoMdAdd } from "react-icons/io";
+import {Badge} from "../components"
+
+const daysOfWeek = ["S", "M", "T", "W", "T", "F", "S"];
+const numRows = 12;
+const numCols = 7;
 
 const Schedule = () => {
   return (
-    <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 py-3 px-5 h-full flex flex-col justify-between">
+    <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-4 px-5 h-full flex flex-col justify-between">
       <div>
         <Row className="pb-4">
-          <Col className="border-2 bg-sky-800 rounded-full max-w-14 text-3xl mx-auto p-2">S</Col>
-          <Col className="border-2 bg-sky-800 rounded-full max-w-14 text-3xl mx-auto p-2">M</Col>
-          <Col className="border-2 bg-sky-800 rounded-full max-w-14 text-3xl mx-auto p-2">T</Col>
-          <Col className="border-2 bg-sky-800 rounded-full max-w-14 text-3xl mx-auto p-2">W</Col>
-          <Col className="border-2 bg-sky-800 rounded-full max-w-14 text-3xl mx-auto p-2">T</Col>
-          <Col className="border-2 bg-sky-800 rounded-full max-w-14 text-3xl mx-auto p-2">F</Col>
-          <Col className="border-2 bg-sky-800 rounded-full max-w-14 text-3xl mx-auto p-2">S</Col>
+          {daysOfWeek.map((day, index) => (
+            <Col key={index} className="border-2 bg-sky-700 border-sky-950 rounded-full max-w-14 text-3xl mx-auto p-2 mb-2">
+              {day}
+            </Col>
+          ))}
         </Row>
-        <Row>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-        </Row>
-        <Row>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-        </Row>
-        <Row>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-        </Row>
-        <Row>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-        </Row>
-        <Row>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-        </Row>
-        <Row>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-          <Col className="border-x-2 text-3xl">.</Col>
-        </Row>
+        <div className="">
+          {[...Array(numRows)].map((_, rowIndex) => (
+            <Row key={rowIndex}>
+              {[...Array(numCols)].map((_, colIndex) => (
+                <Col key={colIndex} className="border-x-2 border-rose-900 text-3xl"><Badge/></Col>
+              ))}
+            </Row>
+          ))}
+        </div>
       </div>
-      <IoMdAdd className="bg-teal-800 hover:bg-teal-950 transition duration-200 p-3 rounded-full mx-auto" color="white" size={80} />
+      <IoMdAdd className="bg-teal-800 hover:bg-teal-950 transition duration-150 p-3 rounded-full mx-auto" color="white" size={80} />
     </Container>
   );
 };
