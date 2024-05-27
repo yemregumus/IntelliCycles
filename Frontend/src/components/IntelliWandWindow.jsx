1f3a351f1df9fb04c3f2caa4b91b9bb744fd1753
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 12:24:08 -0400

    intelliwnad buttons now change color

--- Frontend/src/components/IntelliWandWindow.jsx
+++ Frontend/src/components/IntelliWandWindow.jsx
@@ -5,11 +5,11 @@ import { GiFairyWand } from "react-icons/gi";
 const IntelliWandWindow = () => {
     return (
         <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-3 h-full flex flex-col justify-between">
-            <div className="bg-teal-950 rounded-full text-xl py-3">
+            <div className="bg-sky-800 rounded-full text-xl py-3">
                 IntelliWand
             </div>
             <div className="flex-grow"></div>
-            <GiFairyWand className="bg-teal-950 p-3 rounded-full mx-auto" color="white" size={80}/>
+            <GiFairyWand className="bg-teal-800 hover:bg-teal-950 transition duration-200 p-3 rounded-full mx-auto" color="white" size={80}/>
         </Container>
     );
 }
