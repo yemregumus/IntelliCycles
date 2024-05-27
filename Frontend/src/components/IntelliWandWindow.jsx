70a2c873b0871bc37d1166b4a5be901690319c61
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 14:57:59 -0400

    New comoponent called IntelliWand Window created.

--- Frontend/src/components/IntelliWandWindow.jsx
+++ Frontend/src/components/IntelliWandWindow.jsx
@@ -4,12 +4,12 @@ import { GiFairyWand } from "react-icons/gi";
 
 const IntelliWandWindow = () => {
     return (
-        <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-3 h-full flex flex-col justify-between">
-            <div className="bg-sky-800 rounded-full text-xl py-3">
+        <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-4 h-full flex flex-col justify-between">
+            <div className="bg-sky-700 border-sky-950 border-2 rounded-full text-2xl py-3">
                 IntelliWand
             </div>
             <div className="flex-grow"></div>
-            <GiFairyWand className="bg-teal-800 hover:bg-teal-950 transition duration-200 p-3 rounded-full mx-auto" color="white" size={80}/>
+            <GiFairyWand className="bg-teal-800 hover:bg-teal-950 transition duration-150 p-3 rounded-full mx-auto" color="white" size={80}/>
         </Container>
     );
 }
