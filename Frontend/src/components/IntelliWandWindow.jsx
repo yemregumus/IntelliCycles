8593a95fae964e9a83d0a2761795f6f58348e239
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 12:10:08 -0400

    IntelliWand heading added

--- Frontend/src/components/IntelliWandWindow.jsx
+++ Frontend/src/components/IntelliWandWindow.jsx
@@ -4,11 +4,12 @@ import { GiFairyWand } from "react-icons/gi";
 
 const IntelliWandWindow = () => {
     return (
-        <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-5 h-full flex flex-col justify-between">
-            <div className="flex-grow">
-                hello
+        <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-3 h-full flex flex-col justify-between">
+            <div className="bg-teal-950 rounded-full text-xl py-3">
+                IntelliWand
             </div>
-            <GiFairyWand className="bg-black p-3 rounded-full mx-auto" color="white" size={80}/>
+            <div className="flex-grow"></div>
+            <GiFairyWand className="bg-teal-950 p-3 rounded-full mx-auto" color="white" size={80}/>
         </Container>
     );
 }
