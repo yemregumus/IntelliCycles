6a988c7c367aba5bef6206885eb519bc67ec73ba
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 08:19:08 -0400

    schedule background adjusted

--- Frontend/src/components/Schedule.jsx
+++ Frontend/src/components/Schedule.jsx
@@ -3,7 +3,7 @@ import {Container, Row, Col} from "react-bootstrap"
 
 const Schedule = () =>{
     return(
-        <Container className="text-white text-center my-5">
+        <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-5">
         <Row> 
           <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">S</Col>
           <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">M</Col>
@@ -14,58 +14,58 @@ const Schedule = () =>{
           <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">S</Col>
         </Row>
         <Row>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
         </Row>
         <Row>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
         </Row>
         <Row>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
         </Row>
         <Row>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
         </Row>
         <Row>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
         </Row>
         <Row>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
-          <Col className="border-x-2">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
+          <Col className="border-x-2 text-3xl">.</Col>
         </Row>
       </Container>
     )
