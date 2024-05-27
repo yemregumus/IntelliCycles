385cf291e3945e55ba9849f3ed301f9b3a2cb0a5
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 11:59:01 -0400

    changed height of enclosing divs

--- Frontend/src/components/Schedule.jsx
+++ Frontend/src/components/Schedule.jsx
@@ -3,7 +3,7 @@ import {Container, Row, Col} from "react-bootstrap"
 
 const Schedule = () =>{
     return(
-        <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-5">
+        <Container className="text-white text-center my-5 rounded-3xl bg-zinc-950 bg-opacity-70 p-5 h-full">
         <Row> 
           <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">S</Col>
           <Col className="border-2 bg-emerald-950 rounded-full max-w-14 text-3xl mx-auto">M</Col>
