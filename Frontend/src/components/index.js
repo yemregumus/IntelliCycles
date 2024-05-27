5d4f5941c4c3aaed6c872d4e78be6a557330b7e9
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 14:58:56 -0400

    export statements for IntelliWandWindow and Badge added.

--- Frontend/src/components/index.js
+++ Frontend/src/components/index.js
@@ -1,3 +1,4 @@
 export { default as NavBar } from "./NavBar";
 export { default as Schedule } from "./Schedule";
 export { default as IntelliWandWindow } from "./IntelliWandWindow";
+export {default as Badge } from "./Badge"
