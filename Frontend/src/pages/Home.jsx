422da0f670dc8f0cae22bcd8b803cbb6fa28564b
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-27 11:59:43 -0400

    incorportated Intelliwand window in home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import Schedule from "../components/Schedule";
+import {Schedule, IntelliWandWindow} from "../components";
+import {Row, Col} from "react-bootstrap"
 
 const Home = () => {
     console.log("Rendering Home Page");
     return(
-        <>
-            <Schedule/>
-        </>
+        <div>
+            <Row className="mx-4 h-full">
+                <Col md={2}>
+                    <IntelliWandWindow/>
+                </Col>
+                <Col md={10}>
+                    <Schedule/>
+                </Col>
+            </Row>
+        </div>
     )
     
 }
