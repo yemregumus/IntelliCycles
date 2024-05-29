eb9488a55c04185c14e3d2c2c1369e2df13f18f8
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-29 15:56:32 -0400

    Add Form component started

---
+++ Frontend/src/components/AddForm.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { IoMdCheckmark } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
+const Schedule = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Container className="text-white mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-4 px-5 h-full flex flex-col justify-between">
+      <div>
+        <div className="text-center bg-sky-700 border-sky-950 border-2 rounded-full text-2xl py-3 max-w-52 mx-auto" >
+          ADD
+        </div>
+        <Row>
+          <Col className="text-2xl">
+            NAME
+          </Col>
+          <Col>
+            
+          </Col>
+        </Row>
+      </div>
+      <IoMdCheckmark className="bg-teal-800 hover:bg-teal-950 transition duration-150 p-3 rounded-full mx-auto" color="white" size={80} onClick={() => navigate('/add')} />
+    </Container>
+  );
+};
+
+export default Schedule;
