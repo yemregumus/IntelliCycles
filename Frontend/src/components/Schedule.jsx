6a73c90c7e535b0e016c1b5ca8c3fe4c16d5513c
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-29 15:55:57 -0400

    add button on home now opens add page.

--- Frontend/src/components/Schedule.jsx
+++ Frontend/src/components/Schedule.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { IoMdAdd } from "react-icons/io";
 import {Badge} from "../components"
+import { useNavigate } from "react-router-dom";
 
 const daysOfWeek = ["S", "M", "T", "W", "T", "F", "S"];
 const numRows = 12;
 const numCols = 7;
 
 const Schedule = () => {
+  const navigate = useNavigate();
+
   return (
     <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-4 px-5 h-full flex flex-col justify-between">
       <div>
@@ -28,7 +31,7 @@ const Schedule = () => {
           ))}
         </div>
       </div>
-      <IoMdAdd className="bg-teal-800 hover:bg-teal-950 transition duration-150 p-3 rounded-full mx-auto" color="white" size={80} />
+      <IoMdAdd className="bg-teal-800 hover:bg-teal-950 transition duration-150 p-3 rounded-full mx-auto" color="white" size={80} onClick={() => navigate('/add')} />
     </Container>
   );
 };
