c553f32f57ab0e83fd7f70729a2eaf1021cba27c
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-29 16:30:55 -0400

    Welcome page component created.

---
+++ Frontend/src/pages/Welcome.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Welcome = () => {
+    return(
+        <>
+            <div>
+                Welcome Page
+            </div>
+        </>
+    )
+}
+
+export default Welcome;
--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as Home } from "./Home";
 export { default as Register } from "./Register";
-export { default as Add }from "./Add"
+export { default as Add }from "./Add";
+export {default as Welcome} from "./Welcome";
