222173809f2e6c0c11251ed4fbdad8d885dceeeb
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-30 09:08:31 -0400

    Place holder account page created.

---
+++ Frontend/src/pages/Account.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import {Schedule, IntelliWandWindow} from "../components";
+import {Row, Col} from "react-bootstrap"
+
+const Account = () => {
+    return(
+        <div className="items-center mx-4 h-full text-white text-4xl text-center my-3">
+                Account Page
+        </div>
+    )
+}
+export default Account;
