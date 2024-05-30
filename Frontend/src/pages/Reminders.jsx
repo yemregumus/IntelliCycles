b9672e37e7eb281b7ec64cc831adac1c459a21b9
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-30 09:07:47 -0400

    reminder page framework created.

---
+++ Frontend/src/pages/Reminders.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import {Schedule, IntelliWandWindow} from "../components";
+import {Row, Col} from "react-bootstrap"
+
+const Reminders = () => {
+    return(
+        <div className="items-center">
+            <Row className="mx-4 h-full">
+                <Col md={2}>
+                    <IntelliWandWindow/>
+                </Col>
+                <Col md={10}>
+                    <Schedule type="Reminder"/>
+                </Col>
+            </Row>
+        </div>
+    )
+    
+}
+export default Reminders;
