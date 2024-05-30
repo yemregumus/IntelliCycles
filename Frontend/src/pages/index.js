717a14688cbe28260a74b8d1f8cacd6c7a2aa41a
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-30 09:10:03 -0400

    exports for new pages added.

--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -2,3 +2,8 @@ export { default as Home } from "./Home";
 export { default as Register } from "./Register";
 export { default as Add }from "./Add";
 export {default as Welcome} from "./Welcome";
+export { default as Tasks } from "./Tasks";
+export { default as Habits } from "./Habits";
+export { default as Reminders } from "./Reminders";
+export { default as Calendar } from "./Calendar";
+export { default as Account } from "./Account";
