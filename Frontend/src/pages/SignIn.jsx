37b6c46c3603b8ec6d2429ecd3b7298aa0d5ed70
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-30 13:50:15 -0400

    framework for sign in page added.

---
+++ Frontend/src/pages/SignIn.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const SignIn = () => {
+    return(
+        <div></div>
+    )
+}
+
+export default SignIn
--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -7,4 +7,5 @@ export { default as Habits } from "./Habits";
 export { default as Reminders } from "./Reminders";
 export { default as Calendar } from "./Calendar";
 export { default as Account } from "./Account";
-export { default as IntelliWand } from "./IntelliWand"
+export { default as IntelliWand } from "./IntelliWand";
+export { default as SignIn } from "./SignIn"
