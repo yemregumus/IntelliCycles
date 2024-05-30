0491900bb536ed5cc5f23b5ed62bc018a8a93168
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-30 11:21:41 -0400

    intelliwand heading fixed.

--- Frontend/src/pages/IntelliWand.jsx
+++ Frontend/src/pages/IntelliWand.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import {Container} from "react-bootstrap"
 
 const IntelliWand = () =>{
     return(
-        <div className="items-center mx-4 h-full text-white text-4xl text-center my-3">
-            IntelliWand Page
-        </div>
+        <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-4 px-5 h-full flex flex-col justify-between">
+            <div className="items-center mx-auto min-w-96 h-full text-white text-4xl text-center my-3 border-b-2 max-w-[70rem] pb-3">
+                IntelliWand
+            </div>
+            <div className="h-full">
+                
+            </div>
+        </Container>
     )
 }
 
