f4b68d7a3c08d132c4643b74095fb0412d9fdbc4
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-30 09:24:03 -0400

    Wand button in Intelliwand window now opens the intelliwand page.

--- Frontend/src/components/IntelliWandWindow.jsx
+++ Frontend/src/components/IntelliWandWindow.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 import { GiFairyWand } from "react-icons/gi";
+import { useNavigate } from "react-router-dom";
 
 const IntelliWandWindow = () => {
+    const navigate = useNavigate();
     return (
         <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-4 h-full flex flex-col justify-between">
             <div className="bg-sky-700 border-sky-950 border-2 rounded-full text-2xl py-3">
                 IntelliWand
             </div>
             <div className="flex-grow"></div>
-            <GiFairyWand className="bg-teal-800 hover:bg-teal-950 transition duration-150 p-3 rounded-full mx-auto" color="white" size={80}/>
+            <GiFairyWand className="bg-teal-800 hover:bg-teal-950 transition duration-150 p-3 rounded-full mx-auto" color="white" size={80} onClick={() => navigate('/intelliwand')}/>
         </Container>
     );
 }
