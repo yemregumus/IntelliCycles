dfd119f6ae22162436babd3eae7da04c162e7046
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-05-30 09:08:18 -0400

    placeholder calendar page created.

---
+++ Frontend/src/pages/Calendar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Schedule, IntelliWandWindow} from "../components";
+import {Row, Col} from "react-bootstrap"
+
+const Calendar = () => {
+    return(
+        <div className="items-center mx-4 h-full text-white text-4xl text-center my-3">
+                Calendar Page
+        </div>
+    )
+    
+}
+export default Calendar;
