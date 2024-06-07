ef64940dce518e5e4f58fae23e47ad351e29d3bb
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-06-07 11:18:57 -0400

    Renamed calendar page to event and added + button to event page.

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { getHealthCheck } from "../api";
 import { Home, Register, Add, Welcome, Tasks, Reminders, Habits, 
-  Calendar, Account, IntelliWand, SignIn } from "./pages";
+  Event, Account, IntelliWand, SignIn } from "./pages";
 import { NavBar } from "./components";
 import { Toaster } from "react-hot-toast";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -28,7 +28,7 @@ function App() {
           <Route path="/tasks" element={<RouteGuard><Tasks /></RouteGuard>} />
           <Route path="/reminders" element={<RouteGuard><Reminders /></RouteGuard>} />
           <Route path="/habits" element={<RouteGuard><Habits /></RouteGuard>} />
-          <Route path="/calendar" element={<RouteGuard><Calendar /></RouteGuard>} />
+          <Route path="/calendar" element={<RouteGuard><Event /></RouteGuard>} />
           <Route path="/add/:type" element={<RouteGuard><Add /></RouteGuard>} />
           <Route path="/account" element={<RouteGuard><Account /></RouteGuard>} />
           <Route path="/intelliwand" element={<RouteGuard><IntelliWand /></RouteGuard>} />
