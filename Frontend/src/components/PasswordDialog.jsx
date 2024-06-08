1419c82382f51c180c7bde6aa8d53a24d5691568
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-06-08 10:35:00 -0400

    update password css fixed.

--- Frontend/src/components/PasswordDialog.jsx
+++ Frontend/src/components/PasswordDialog.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Modal, Button, Form, Container } from 'react-bootstrap';
+import { Modal, Button, Form } from 'react-bootstrap';
 import { toast } from 'react-hot-toast';
 import { getUserIdFromToken, getToken } from '../utils/auth';
 
@@ -39,59 +39,61 @@ function PasswordDialog({show, handleClose, apiUrl}) {
         }
     };
 
-  return (
-    <div>
-         <Modal show={show} onHide={handleClose} centered >
-            <Modal.Dialog className="w-full h-full m-0">
-                <div className="text-white text-center rounded-3xl bg-zinc-950 bg-opacity-90 p-3 h-full">
-                    <Modal.Header closeButton>
-                        <Modal.Title>Change Password</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <Form>
-                            <Form.Group className="mb-3" controlId="oldPassword">
-                                <Form.Label>Old Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Enter old password"
-                                    value={oldPassword}
-                                    onChange={(e) => setOldPassword(e.target.value)}
-                                    className="bg-black text-white placeholder-stone-400 rounded-full"
-                                />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="newPassword">
-                                <Form.Label>New Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Enter new password"
-                                    value={newPassword}
-                                    onChange={(e) => setNewPassword(e.target.value)}
-                                    className="bg-black text-white placeholder-stone-400 rounded-full"
-                                />
-                            </Form.Group>
-                            <Form.Group className="mb-3" controlId="confirmPassword">
-                                <Form.Label>Confirm New Password</Form.Label>
-                                <Form.Control
-                                    type="password"
-                                    placeholder="Confirm new password"
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                    className="bg-black text-white placeholder-stone-400 rounded-full"
-                                />
-                            </Form.Group>
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Cancel
-                        </Button>
-                        <Button variant="primary" onClick={handlePasswordChange}>
-                            Change Password
-                        </Button>
-                    </Modal.Footer>
-                </div>
-            </Modal.Dialog>
-        </Modal> 
-    </div> 
-)}
+    return (
+        <div>
+            <Modal show={show} onHide={handleClose} centered>
+                <Modal.Dialog className="w-full h-full m-0 bg-zinc-950">
+                    <div className="text-white border-3 border-amber-900 text-center bg-black p-3 h-full">
+                        <Modal.Header closeVariant='white' closeButton>
+                            <Modal.Title>Change Password</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body className='p-4'>
+                            <Form>
+                                <Form.Group className="mb-4 flex items-center" controlId="oldPassword">
+                                    <Form.Label className="w-2/6 text-left">Old Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Enter old password"
+                                        value={oldPassword}
+                                        onChange={(e) => setOldPassword(e.target.value)}
+                                        className="w-5/6 bg-black text-white placeholder-stone-400 rounded-full"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="mb-3 flex items-center" controlId="newPassword">
+                                    <Form.Label className="w-2/6 text-left">New Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Enter new password"
+                                        value={newPassword}
+                                        onChange={(e) => setNewPassword(e.target.value)}
+                                        className="w-5/6 bg-black text-white placeholder-stone-400 rounded-full"
+                                    />
+                                </Form.Group>
+                                <Form.Group className="flex items-center" controlId="confirmPassword">
+                                    <Form.Label className="w-2/6 text-left">Confirm New Password</Form.Label>
+                                    <Form.Control
+                                        type="password"
+                                        placeholder="Confirm new password"
+                                        value={confirmPassword}
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
+                                        className="w-5/6 bg-black text-white placeholder-stone-400 rounded-full"
+                                    />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="danger" onClick={handleClose}>
+                                Cancel
+                            </Button>
+                            <Button variant="success" onClick={handlePasswordChange}>
+                                Change Password
+                            </Button>
+                        </Modal.Footer>
+                    </div>
+                </Modal.Dialog>
+            </Modal> 
+        </div>
+    );
+}
+
 export default PasswordDialog;
