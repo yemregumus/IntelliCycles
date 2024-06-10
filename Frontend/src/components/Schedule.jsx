d21c189917e92e6614b4f083cfdbcac3c307508d
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-06-10 15:27:08 -0400

    page titles added

--- Frontend/src/components/Schedule.jsx
+++ Frontend/src/components/Schedule.jsx
@@ -5,15 +5,17 @@ import {Badge} from "../components"
 import { useNavigate } from "react-router-dom";
 
 const daysOfWeek = ["S", "M", "T", "W", "T", "F", "S"];
-const numRows = 12;
+const numRows = 11;
 const numCols = 7;
 
-const Schedule = ({type}) => {
+const Schedule = ({type=""}) => {
   const navigate = useNavigate();
+  const title = type ? `${type.toUpperCase()}S` : "HOME";
 
   return (
-    <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-4 px-5 h-full flex flex-col justify-between">
+    <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-3 px-5 h-full flex flex-col justify-between">
       <div>
+        <div className="text-3xl border-b-2 text-center pb-3 my-4 w-2/5 mx-auto">{title}</div>
         <Row className="pb-4">
           {daysOfWeek.map((day, index) => (
             <Col key={index} className="backdrop-blur-sm bg-white/30 rounded-full max-w-14 text-3xl mx-auto p-2 mb-2">
@@ -25,7 +27,7 @@ const Schedule = ({type}) => {
           {[...Array(numRows)].map((_, rowIndex) => (
             <Row key={rowIndex}>
               {[...Array(numCols)].map((_, colIndex) => (
-                <Col key={colIndex} className="border-x-2 border-rose-900 text-3xl"><Badge type= {type}/></Col>
+                <Col key={colIndex} className="border-x-2 border-rose-900 text-3xl"><Badge type= {title}/></Col>
               ))}
             </Row>
           ))}
