82e68a16eac7763635bb7bf30de73665e4326aac
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-06-11 17:42:55 -0400

    Badge now displays task name

--- Frontend/src/components/Badge.jsx
+++ Frontend/src/components/Badge.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import React from "react";
 import EditDialog from "./EditForms/EditDialog"
 
-const Badge = ({type}) =>{
+const Badge = ({type, task}) =>{
     const [showEditDialog, setShowEditDialog] = useState(false);
     type=type.toLowerCase()
     return(
         <div>
             <div  onClick={() => setShowEditDialog(true)} className="text-lg rounded-full bg-orange-200 hover:bg-orange-400 text-black transition duration-150 m-1 p-1">
-                {type ? type : "HOME"}
+                {task.name}
             </div>
-            <EditDialog show={showEditDialog} type={type ? type : "task"} handleClose={() => setShowEditDialog(false)}/>
+            <EditDialog show={showEditDialog} type={type ? type : "task"} task={task} handleClose={() => setShowEditDialog(false)}/>
         </div>
     )
 }
