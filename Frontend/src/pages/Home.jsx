966267de90cdfc636485244d2bbe4b0f008c19a3
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-06-12 14:29:38 -0400

    length of containers changed.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,8 +4,8 @@ import {Row, Col} from "react-bootstrap"
 
 const Home = () => {
     return(
-        <div className="my-3 h-screen">
-            <Row className="mx-4 h-5/6">
+        <div className="my-2 h-[50rem]">
+            <Row className="mx-4 h-[49rem]">
                 <Col md={2}>
                     <IntelliWandWindow/>
                 </Col>
