9906e2cc802dc77536331d2e73221faaafd5416b
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-06-12 14:49:15 -0400

    Home page done

--- Frontend/src/components/Schedule.jsx
+++ Frontend/src/components/Schedule.jsx
@@ -39,28 +39,33 @@ const Schedule = ({ type = "" }) => {
     fetchTasks();
   }, [userid]);
 
-  // Organize tasks by day of the week
+  // Organize tasks by day of the week and sort each day's tasks by due date and time
   const tasksByDay = daysOfWeek.map(() => []);
   tasks.forEach(task => {
     const day = moment(task.due_date).day();
     tasksByDay[day].push(task);
   });
 
+  // Sort tasks within each day
+  tasksByDay.forEach(dayTasks => {
+    dayTasks.sort((a, b) => new Date(a.due_date) - new Date(b.due_date));
+  });
+
   // Calculate the maximum number of tasks in any given day
   const maxTasks = Math.max(...tasksByDay.map(dayTasks => dayTasks.length));
 
   return (
-    <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-3 px-5 h-full flex flex-col justify-between">
+    <Container className="text-white text-center mt-4 rounded-3xl bg-zinc-950 bg-opacity-60 p-3 px-2 h-full flex flex-col justify-between">
       <div>
         <div className="text-3xl border-b-2 text-center pb-3 my-4 w-2/5 mx-auto">{title}</div>
-        <div className="overflow-y-auto overflow-x-hidden p-3">
-          <Row className="pb-4">
+          <Row className="pb-4 px-4">
             {daysOfWeek.map((day, index) => (
               <Col key={index} className="backdrop-blur-sm bg-white/30 rounded-full max-w-14 text-3xl mx-auto p-2 mb-2">
                 {day.charAt(0)}
               </Col>
             ))}
           </Row>
+        <div className="overflow-y-auto h-[30rem] overflow-x-hidden p-4">
           <div className="">
             {[...Array(maxTasks)].map((_, rowIndex) => (
               <Row key={rowIndex}>
