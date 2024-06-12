81a93a40ec0137c485a545ccad942206528b76c1
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-06-12 16:18:40 -0400

    max width added to badges

--- Frontend/src/components/Badge.jsx
+++ Frontend/src/components/Badge.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import React from "react";
 import EditDialog from "./EditForms/EditDialog"
 
-const Badge = ({type, task}) =>{
+const Badge = ({type, entity}) =>{
     const [showEditDialog, setShowEditDialog] = useState(false);
     type=type.toLowerCase()
     return(
         <div>
-            <div  onClick={() => setShowEditDialog(true)} className="text-lg rounded-full bg-orange-200 hover:bg-orange-400 text-black transition duration-150 m-1 p-1">
-                {task.name}
+            <div  onClick={() => setShowEditDialog(true)} className=" max-w-[7rem] text-lg rounded-full bg-orange-200 hover:bg-orange-400 text-black transition duration-150 m-1 p-1">
+                {entity.name}
             </div>
-            <EditDialog show={showEditDialog} type={type ? type : "task"} task={task} handleClose={() => setShowEditDialog(false)}/>
+            <EditDialog show={showEditDialog} type={type ? type : "task"} entity={entity} handleClose={() => setShowEditDialog(false)}/>
         </div>
     )
 }
