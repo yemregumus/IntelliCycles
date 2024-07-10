b073a762b192a3b76d1be1c3eb8a666f16d7ee54
Toyosi Kuti <okuti2@myseneca.ca>
2024-07-10 09:55:33 -0400

    Editing and deleting events from calendar working, minor bugs with date and timezone

--- Frontend/src/components/Calendar.jsx
+++ Frontend/src/components/Calendar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { formatDate } from '@fullcalendar/core';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -7,11 +7,19 @@ import interactionPlugin from "@fullcalendar/interaction";
 import listPlugin from "@fullcalendar/list";
 import multiMonthPlugin from '@fullcalendar/multimonth';
 import { EditDialog } from './EditForms';
+import { getUserIdFromToken } from '../utils/auth';
+import { ConeStriped } from 'react-bootstrap-icons';
+import { getActivitiesByUser } from '../../api';
+import { toast } from 'react-hot-toast';
+import { all } from 'axios';
 
 function Calendar() {
 
   const [currentEvents, setCurrentEvents] = useState([]);
+  const [tasksUpdated, setTasksUpdated] = useState(false);
+  const [event, setEvent] = useState(null);
   const [showEditDialog, setShowEditDialog] = useState(false);
+  const userId = getUserIdFromToken();
 
   const handleDateClick = (selected) => {
     const title = prompt("Please enter a new title for your event");
@@ -28,6 +36,71 @@ function Calendar() {
       });
     }
   };
+
+  const handleEventClick = (info) => {
+    console.log(info.event);
+
+    const startDate = info.event.start instanceof Date ? info.event.start.toISOString() : info.event.start;
+    const endDate = info.event.end instanceof Date ? info.event.end.toISOString() : info.event.end;
+
+    console.log(startDate);
+    console.log(endDate);
+
+    const selectedEvent = {
+      id: info.event.id,
+      name: info.event.title,
+      description: info.event.extendedProps.description,
+      startDateTime: startDate,
+      endDateTime: endDate,
+      color: info.event.backgroundColor,
+      repeatInterval: info.event.extendedProps.interval,
+      type: info.event.extendedProps.type,
+    };
+    setEvent(selectedEvent);
+    setShowEditDialog(true)
+  };
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const entities = await getActivitiesByUser(userId);
+        if (entities) {
+          if (entities.length === 0) {
+            toast.success("You currently do not have any events set");
+          } else {
+
+            // filter out only events
+            const validEntities = entities.filter(entity => entity.endDateTime);
+
+            const calendarEvents = validEntities.map(entity => ({
+              id: entity.id,
+              title: entity.name,
+              type: entity.type,
+              start: entity.startDateTime,
+              end: entity.endDateTime,
+              color: entity.color,
+              interval: entity.repeatInterval,
+              description: entity.description,
+              allDay: true,
+              // You can add more properties here as needed
+            }));
+      
+            // Sort tasks by due date and time
+            //calendarEvents.sort((a, b) => new Date(a.start) - new Date(b.start));
+            setCurrentEvents(calendarEvents);
+            console.log("Fetched tasks before map:", validEntities);
+            console.log("Fetched tasks:", calendarEvents);
+
+          
+          }
+        }
+      } catch (error) {
+        console.error('Failed to fetch events:', error);
+      }
+    };
+    fetchEvents();
+  
+  }, [userId, tasksUpdated]);
 
   return (
     <div className="m-5">
@@ -53,24 +126,18 @@ function Calendar() {
                     selectMirror={true}
                     dayMaxEvents={true}
                     select={handleDateClick}
-                    eventClick={() => setShowEditDialog(true)}
-                    eventsSet={(events) => setCurrentEvents(events)}
-                    initialEvents={[
-                        {
-                            id: "12315",
-                            title: "Task: Do Assignment",
-                            date: "2024-06-14",
-                        },
-                        {
-                            id: "5123",
-                            title: "Habit: Drink Water",
-                            date: "2024-06-28",
-                        },
-                    ]}
+                    eventClick={handleEventClick}
+                    events={currentEvents}
                 />
             </div>
         </div>
-        <EditDialog show={showEditDialog} type='event' handleClose={() => setShowEditDialog(false)} />
+        <EditDialog
+                show={showEditDialog}
+                type="event"
+                entity={event}
+                handleClose={() => setShowEditDialog(false)}
+                updateTasks={() => setTasksUpdated(!tasksUpdated)}
+            />
     </div>
   )
 }
