ca45ba5a191aa1d27db2e0dd3e17fb328cfcb600
Hashmeet Singh Saini <hsaini28@myseneca.ca>
2024-07-31 11:07:19 -0400

    chat bot api functions added.

--- Frontend/api/index.js
+++ Frontend/api/index.js
@@ -1,8 +1,11 @@
 import { getHealthCheck } from "./healthCheck";
 import { createActivity, getActivitiesByUser, deleteActivityById, updateActivityById } from "./activity";
+import { getQuestions, getAnswer } from "./chatbot";
 
 export { getHealthCheck };
 export { createActivity };
 export { getActivitiesByUser };
 export { deleteActivityById };
 export { updateActivityById };
+export { getQuestions };
+export { getAnswer };
